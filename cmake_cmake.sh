629a42fd34df0a096eb9fe8dd72863af73d1500f
Horea Caramizaru <horea.caramizaru@cti.pub.ro>
2017-06-28 00:56:54 +0300

    improve automation build by adding install system for mmtk_environment based on anaconda. Also, build proces in done in 3 steps in build-debug: (1) bash ../cmake_install_dep.sh (2) suource activate mmtk_environment bash ../cmake_cmake.sh . I've done 2 helper scripts just so you don't have to think about 2 environment variable from cmake, but still you'll have to set in cmake_install_dep.sh anaconda envs folder path

---
+++ cmake_cmake.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ cmake .. -DMakeDownload_mmtk_environment=false -DInstallEnvironment=false -DAnacondaEnvironmentPath="/home/nash_mit/Anaconda/envs/"
---
+++ cmake_install_dep.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cmake .. -DMakeDownload_mmtk_environment=true -DInstallEnvironment=true -DAnacondaEnvironmentPath="/home/nash_mit/Anaconda/envs/"
+
