1729083ffd8c6f13ab001e837b330971945daa47
Horea Caramizaru <horea.caramizaru@cti.pub.ro>
2017-06-28 15:21:27 +0300

    gfortran libglfw3-dev freeglut3-dev freeglut3-dev libxmu-dev libeigen3-dev doxygen libblas-dev liblapack-dev libboost-all-dev

--- cmake_install_dep.sh
+++ cmake_install_dep.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+
+sudo apt-get install gfortran
+sudo apt-get install libglfw3-dev
+sudo apt-get install freeglut3-dev
+sudo apt-get install freeglut3-dev
+sudo apt-get install libxmu-dev
+sudo apt-get install libeigen3-dev
+sudo apt-get install doxygen
+sudo apt-get install libblas-dev liblapack-dev
+sudo apt-get install libboost-all-dev
+
 cmake .. -DMakeDownload_mmtk_environment=true -DInstallEnvironment=true -DAnacondaEnvironmentPath="/home/nash_mit/Anaconda/envs/"
 
