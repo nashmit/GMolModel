af8ab6859c8fadcdac5dd0a77c93e9792d04d55c
spiridon_laurentiu <spiridon.laurentiu@gmail.com>
2017-07-17 19:06:14 +0300

    MolecularMechanicsSystem segfault

--- tests/native.cpp
+++ tests/native.cpp
@@ -1,9 +1,62 @@
+/* -------------------------------------------------------------------------- *
+ *                 SimTK Molmodel Example: Simple Protein                     *
+ * -------------------------------------------------------------------------- *
+ * This is the first example from the Molmodel User's Guide. It creates a     *
+ * small protein (a five-residue peptide), simulates it and generates a live  *
+ * animation while it is running.                                             *
+ *                                                                            *
+ * Authors: Christopher Bruns, Michael Sherman                                *
+ * -------------------------------------------------------------------------- */
+
+#include "Molmodel.h"
 #include <iostream>
+#include <exception>
+using namespace SimTK;
 
-int main(int argc, char **argv)
-{
+int main() {
+try {
+    
+    MultibodySystem someMultibodySystem;
+    //MolecularMechanicsSystem someMolecularMechanicsSystem;
+    //CompoundSystem system;
+    //SimbodyMatterSubsystem matter(system);
+    //DecorationSubsystem decorations(system);
 
-    std::cout<<"Hello World!"<<std::endl;
+    //DuMMForceFieldSubsystem forceField(system);
+    //forceField.loadAmber99Parameters();
+
+    //Protein protein("SIMTK");
+
+    //protein.assignBiotypes();
+    //system.adoptCompound(protein);
+
+    //system.modelCompounds(); 
+
+    //system.addEventReporter(new Visualizer::Reporter(system, 0.020));
+
+    //system.addEventHandler(new VelocityRescalingThermostat(	   system,  293.15, 0.1));
+
+    //State state = system.realizeTopology();
+
+    //LocalEnergyMinimizer::minimizeEnergy(system, state, 15.0);
+
+    //VerletIntegrator integ(system);
+    //TimeStepper ts(system, integ);
+    //ts.initialize(state);
+    //ts.stepTo(20.0); // 20ps
+
     return 0;
+} 
+catch(const std::exception& e) {
+    std::cerr << "ERROR: " << e.what() << std::endl;
+    return 1;
+}
+catch(...) {
+    std::cerr << "ERROR: An unknown exception was raised" 
+              << std::endl;
+    return 1;
+}
 
 }
+
+
