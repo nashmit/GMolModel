10f4ad37803c7ba3cb3bec2c7bc807d9b92e90e1
spiridon_laurentiu <spiridon.laurentiu@gmail.com>
2017-07-18 15:04:36 +0300

    Simbody constructors work, Molmodel's don't.

--- tests/TestNative.cpp
+++ tests/TestNative.cpp
@@ -16,32 +16,32 @@ using namespace SimTK;
 int main() {
 try {
     
-    //MultibodySystem someMultibodySystem;
-    MolecularMechanicsSystem someMolecularMechanicsSystem;
-    //CompoundSystem system;
+    // SIMBODY CONSTRUCTORS WORK:
+    System someSystem;
+    MultibodySystem someMultibodySystem; // Extends System (Simbody) // adoptSystemGuts(new MultibodySystemRep())
+    SimbodyMatterSubsystem matter(someMultibodySystem); // Extends Subsystem (Simbody)
+    DecorationSubsystem decorations(someMultibodySystem); // Extends Subsystem (Simbody)
+    // =====================
+
+    // MOLMODEL CONSTRUCTORS DON'T WORK:
+    //MolecularMechanicsSystem someMolecularMechanicsSystem; // Extends MultibodySystem (Simbody)
+    //CompoundSystem system; // Extends MolecularMechanicsSystem (Molmodel)
     //SimbodyMatterSubsystem matter(system);
     //DecorationSubsystem decorations(system);
-
     //DuMMForceFieldSubsystem forceField(system);
     //forceField.loadAmber99Parameters();
-
     //Protein protein("SIMTK");
-
     //protein.assignBiotypes();
     //system.adoptCompound(protein);
-
     //system.modelCompounds(); 
-
     //system.addEventReporter(new Visualizer::Reporter(system, 0.020));
-
     //system.addEventHandler(new VelocityRescalingThermostat(	   system,  293.15, 0.1));
-
     //State state = system.realizeTopology();
-
     //LocalEnergyMinimizer::minimizeEnergy(system, state, 15.0);
-
     //VerletIntegrator integ(system);
     //TimeStepper ts(system, integ);
+    // =====================
+
     //ts.initialize(state);
     //ts.stepTo(20.0); // 20ps
 
