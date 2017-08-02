4f3435a098a09ba7333ac7b7c482734d2e1ce63d
spiridon_laurentiu <spiridon.laurentiu@gmail.com>
2017-08-02 15:52:34 +0300

    Context class first commit. Only CMakeList.txt modified.

---
+++ tests/TestContext.cpp
@@ -0,0 +1,15 @@
+/** @file
+ * This file checks the basic functionality of Context class.
+ */
+
+#include <string>
+#include <iostream>
+#include <sstream>
+#include "simmain.hpp"
+
+
+int main(int argc, char **argv){
+
+
+    return 0;
+}
