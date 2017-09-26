59576ee64671f177c6fa3bd6c05bbba429611335
spiridon_laurentiu <spiridon.laurentiu@gmail.com>
2017-09-26 13:43:49 +0300

    TestPrmCrd.cpp file added.

--- tests/TestPrmCrd.cpp
+++ tests/TestPrmCrd.cpp
@@ -127,172 +127,66 @@ int main(int argc, char **argv)
     );
 
 
-    // Test Context 1
-    //Sampler *p_genericSampler = new Sampler(p_world->system, p_world->matter, p_world->lig1, p_world->ts);
-    //HamiltonianMonteCarloSampler *p_HMCsampler = new HamiltonianMonteCarloSampler(p_world->system, p_world->matter, p_world->lig1, p_world->ts);
-    //Context *context = new Context(p_world, p_HMCsampler);
-    //World *world = context->getWorld();
-
-    // Memory alloc for convinient arrays 
-
-    coords = new TARGET_TYPE*[p_world->mr->natms];
-    TARGET_TYPE **vels = new TARGET_TYPE*[p_world->mr->natms];
-    TARGET_TYPE **inivels = new TARGET_TYPE*[p_world->mr->natms];
-    TARGET_TYPE **grads = new TARGET_TYPE*[p_world->mr->natms];
-
     // Seed the random number generator 
 
     srand (time(NULL));
 
     // Load initial shared memory values
 
-    float mysweep = 0;
-    shm[0] = CNT_START;
-
-    int cli;
-    sweep = 0;
     bool world_initialized = false;
-    int big_loop_i;
-
-    shm[1] = CLIENT_FLAG;
-    int a;
-    int start, start_1, stop;
-    int i = natoms*2*3 + 2;
 
     // Assign convenient pointers for order
 
-    start = 2 + natoms3 + natoms3; 
-    for(a=0; a<natoms; a++){
-        indexMap[a] = &(shm[a*3 + start]);
-    }
-
     // Assign order in shm[][2] 
-
-    start = 2 + natoms3 + natoms3;
-    cli = start;
-    for(a=0; a<natoms; a++){ // Order
-        cli += 2;
-        shm[cli++] = order[a];
-    }
-
-    cli = 2;
 
     // Get coordinates in the same order as in mol2
 
-    int mol2atomlineno = -1;
-    int mol2atom_no;
-    std::string mol2atom_name;
-    SimTK::Real mol2x, mol2y, mol2z;
-
-    mol2ifstream.open(mol2FN);
-    while(std::getline(mol2ifstream, line)){
-        std::istringstream iss(line);
-        iss >> column;
-        if(column == "@<TRIPOS>ATOM") break;
-    }
-    while(std::getline(mol2ifstream, line)){ // WATCHOUT to read only natoms lines
-        ++mol2atomlineno;
-        std::istringstream iss(line);
-        column = line.substr(0, 13);
-        if((column == "@<TRIPOS>BOND") || (mol2atomlineno == natoms)) break;
-        iss >> mol2atom_no;
-        iss >> mol2atom_name;
-        iss >> mol2x;
-        iss >> mol2y;
-        iss >> mol2z;
-        std::cout<<"mol2 info "<<mol2atom_no<<" "<< mol2atom_name
-            <<" "<<mol2x<<" "<<mol2y<<" "<<mol2z<<std::endl;
-        shm[cli++] = mol2x/10.0; shm[cli++] = mol2y/10.0; shm[cli++] = mol2z/10.0; // convert A to nm
-    }
-    mol2ifstream.close();
-
     // Get velocities
-
-    for(a=0; a<natoms; a++){
-        shm[cli++] = .0; 
-        shm[cli++] = .0; 
-        shm[cli++] = .0; 
-    }
 
     // Get order    
 
-    for(a=0; a<natoms; a++){
-        cli += 2;
-        shm[cli++] = order[a];
-    }
-
     // Get forces   
-
-    for(a=0; a<natoms; a++){
-        shm[cli++] = .0;
-        shm[cli++] = .0;
-        shm[cli++] = .0;
-    }
 
     // Get simulation parameters
 
-    shm[cli++] = big_loop_i;          // Step
-    shm[cli++] = (TARGET_TYPE)nosteps;    // Number of steps
-    shm[cli++] = temperature; // Temeperature
-    shm[cli++] = delta_t;
-    shm[cli++] = 0.0; // Trouble flag
-    shm[cli++] = 0.0; // p_energy_po->energy from MMTK
-    cli++; // DAE set only by the server
-    shm[cli] = acceptance + 0.0;
-
     // Set the client flag
-
-    shm[1] = CLIENT_FLAG;
-    shm[arrays_cut + 6] = 13.0; // set DAE to done
 
     // Assign convenient pointers for coordinates
 
-    int shm_coords_sup = (natoms3)+2;
-    int j=-1, k=0;
-    start = 2; 
-    for(j=0; j<natoms; j++){
-        coords[j] = &(shm[j*3 + start]);
-    }
-
-    mysweep = shm[0] - CNT_START;
-
     // Assign convenient pointers for velocities
-
-    start = 2 + natoms3;
-    start_1 = start - 1;
-    stop = 2 + natoms3 + natoms3;
-    for(j=0; j<natoms; j++){
-        vels[j]    = &(shm[j*3 + start]);
-        inivels[j] = &(shm[j*3 + start]);
-    }
 
     // Assign convenient pointers for gradients
 
-    start = 2 + natoms3 + natoms3 + natoms3;
-    start_1 = start - 1;
-    stop = 2 + natoms3 + natoms3 + natoms3 + natoms3;
-    for(j=0; j<natoms; j++){
-        grads[j] = &(shm[j*3 + start]);
-    }
-
     // Rewrite indexMap to memory
-
-    start = 2 + natoms3 + natoms3;
-    start_1 = start - 1;
-    stop = 2 + natoms3 + natoms3 + natoms3;
 
     // Initialize Simulation
 
     TARGET_TYPE timestep, mytimestep;
-    mytimestep = shm[arrays_cut + 3];
+    mytimestep = 0.0015;
 
-    // Build Topology and fill indexMap
+    // Aloc necessary memory for InitSimulation - temporary
+
+    SimTK::Real **coords;
+    coords = new SimTK::Real*[p_world->mr->natms]; // ELIZA
+    for(int j=0; j<natoms; j++){coords[j] = new SimTK::Real[3];}
+    SimTK::Real **vels;
+    vels = new SimTK::Real*[p_world->mr->natms]; // ELIZA
+    for(int j=0; j<natoms; j++){vels[j] = new SimTK::Real[3];}
+    SimTK::Real **inivels;
+    inivels = new SimTK::Real*[p_world->mr->natms]; // ELIZA
+    for(int j=0; j<natoms; j++){inivels[j] = new SimTK::Real[3];}
+    SimTK::Real **indexMap;
+    indexMap = new SimTK::Real*[p_world->mr->natms]; // ELIZA
+    for(int j=0; j<natoms; j++){indexMap[j] = new SimTK::Real[3];}
+    SimTK::Real **grads;
+    grads = new SimTK::Real*[p_world->mr->natms]; // ELIZA
+    for(int j=0; j<natoms; j++){grads[j] = new SimTK::Real[3];}
+
 
     p_world->InitSimulation(coords, vels, inivels, indexMap, grads, mytimestep, true);
     world_initialized = true;
 
-    // Test Context 2
-    Sampler *p_genericSampler = new Sampler(p_world->system, p_world->matter, p_world->lig1, p_world->ts);
+    // Initialize sampler
     HamiltonianMonteCarloSampler *p_HMCsampler = new HamiltonianMonteCarloSampler(p_world->system, p_world->matter, p_world->lig1, p_world->ts);
     Context *context = new Context(p_world, p_HMCsampler);
     World *world = context->getWorld();
@@ -305,10 +199,6 @@ int main(int argc, char **argv)
     int _massMatNumOpt = 1; // EU
     int _metroFixmanOpt = 1; // EU
     double _lj14sf = 1; //--
-
-    #ifdef DEBUG_TIME
-            //boost::timer boost_timer;
-    #endif
 
     // Alloc memory for saving configurations WATCHOUT TOO BIG
 
@@ -326,23 +216,6 @@ int main(int argc, char **argv)
     world->sysRetConfsPois = retConfsPois;
     world->sysRetPotEsPoi = retPotEsPoi;
     world->sysAccs = accs;
-
-    arrays_cut = 2 + 4*3*(world->mr->natms);
-    shm[arrays_cut + 0] = 0.0; // step (will be incremented in MidVV
-    shm[arrays_cut + 1] = (TARGET_TYPE)(nosteps);
-    shm[arrays_cut + 2] = temperature;
-    shm[arrays_cut + 3] = delta_t; // picosec
-    shm[arrays_cut + 7] = 1.0;     // acceptance always 1 !!
-    shm[arrays_cut + 9] = (TARGET_TYPE)steps_per_trial; // steps_per_trial
-    shm[arrays_cut + 10] = 0.0;    // initialize trial
-
-    // Print shm
-    std::cout<<"shm contents:"<<std::endl;
-    for(i=0; i<SHMSZ; i++){
-        std::cout<<shm[i]<<" ";
-        if(i%3 == 0) std::cout<<std::endl;
-    }
-    std::cout<<std::endl;
 
    // Simulate
 
