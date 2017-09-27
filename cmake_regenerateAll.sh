ed9ce2e322780a4e952082cc2a3600952da05985
Horea Caramizaru <horea.caramizaru@cti.pub.ro>
2017-09-27 11:15:55 +0300

    add cmake_regenerateAll.sh

---
+++ cmake_regenerateAll.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd ../molmodel_legacy/simbody
+mkdir build-debug
+cd build-debug
+
+cmake ..
+make -j4
+sudo make install
+
+cd ../../
+mkdir build-debug
+cd build-debug
+cmake ..
+make -j4
+sudo make install
+
+cd ../../openmm
+mkdir build-debug
+cd build-debug
+cmake ..
+make -j4
+sudo make install
+
+cd ../../build-debug/
+
+
+cmake ..
+
