1b3056f21660f70e38e77f11367fa2a6deb04569
spiridon_laurentiu <spiridon.laurentiu@gmail.com>
2017-09-28 21:10:24 +0300

    TestPDBReaderMolModel.cpp update.

--- tests/TestPDBReaderMolModel.cpp
+++ tests/TestPDBReaderMolModel.cpp
@@ -4,6 +4,7 @@
 #include <exception>
 
 #include "HamiltonianMonteCarloSampler.hpp"
+#include "World.hpp"
 
 using namespace SimTK;
 
@@ -12,7 +13,7 @@ try {
     // Load the PDB file and construct the system.
     CompoundSystem system;
     SimbodyMatterSubsystem matter(system);
-    DecorationSubsystem decorations(system);
+    //DecorationSubsystem decorations(system);
     DuMMForceFieldSubsystem forceField(system);
     forceField.loadAmber99Parameters();
 
@@ -20,12 +21,12 @@ try {
     pdb.createCompounds(system);
     system.modelCompounds();
 
-    system.addEventHandler(new VelocityRescalingThermostat(
-        system, 293.15, 0.1));
+    //system.addEventHandler(new VelocityRescalingThermostat(
+    //    system, 293.15, 0.1));
 
     // Show me a movie
-    Visualizer viz(system);
-    system.addEventReporter( new Visualizer::Reporter(viz, 0.025) );
+    //Visualizer viz(system);
+    //system.addEventReporter( new Visualizer::Reporter(viz, 0.0015) );
 
     system.realizeTopology();
     
@@ -47,7 +48,29 @@ try {
 	// CHANGES
 	HamiltonianMonteCarloSampler *p_HMCsampler = new HamiltonianMonteCarloSampler(&system, &matter, &system.updCompound( SimTK::CompoundSystem::CompoundIndex(0) ), &ts);
 
+    SimTK::State& integAdvancedState = integ.updAdvancedState();
+    for(int i=0; i<30; i++){
+        std::cout << "=========================================" << std::endl;
+        std::cout << "Q before update integAdvancedState "
+                  << integAdvancedState.getQ() << std::endl;
+        std::cout << "U before update integAdvancedState "
+                  << integAdvancedState.getU() << std::endl;
+        std::cout << "Time before update: " << ts.getTime() << std::endl;
 
+        p_HMCsampler->update(integAdvancedState);
+
+        std::cout << "Q after update integAdvancedState "
+                  << integAdvancedState.getQ() << std::endl;
+        std::cout << "Time after update: " << ts.getTime()
+                  //<< "; integAdvancedState Stage after p_HMCsampler: "
+                  //<< (((SimTK::Subsystem)(matter)).getStage(integAdvancedState)).getName()
+                  //<< "; integAdvancedState Stage before stepping: "
+                  //<< (((SimTK::Subsystem)(matter)).getStage(integAdvancedState)).getName()
+                  << std::endl;
+        //writePdb(*((SimTK::Compound *)(world->lig1)), integAdvancedState, "pdbs", "sb_", 8, "MCs", i);
+        writePdb(system.updCompound( SimTK::CompoundSystem::CompoundIndex(0) ), integAdvancedState, "pdbs", "sb_", 8, "HMCs", i);
+
+    }
 
     return 0;
 
