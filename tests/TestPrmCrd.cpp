46d591908a612275737697e8f77324287244c20c
spiridon_laurentiu <spiridon.laurentiu@gmail.com>
2017-10-05 12:44:52 +0300

    Deleted TestHamiltonianMonteCarlo.cpp.

--- tests/TestPrmCrd.cpp
+++ tests/TestPrmCrd.cpp
@@ -17,24 +17,18 @@ int main(int argc, char **argv)
 
     TARGET_TYPE *shm;
     int SHMSZ; // size
-    int sweep;
+    //int sweep;
 
     //  Declare variables for simulation parameters
 
     int natoms;
     int nosteps;
     int ntrials;
-    int steps_per_trial;
-    double temperature;
-    double delta_t;
 
     // Set simulation parameters
 
-    temperature = 300.0;
-    delta_t = 0.0015; // ps
     nosteps = 10; // RESTORE DEL
     ntrials = 10; // RESTORE DEL
-    steps_per_trial = nosteps / ntrials;
     std::cout<<"main ntrials: "<<ntrials<<std::endl;
     std::cout<<"main nosteps: "<<nosteps<<std::endl;
 
@@ -86,30 +80,16 @@ int main(int argc, char **argv)
 
     // Read atom ordering from mol2
  
-    int order[natoms+2]; // prmtop ORDER previously read from MMTK
-    int acceptance;
     TARGET_TYPE **indexMap = NULL;
     TARGET_TYPE *PrmToAx_po = NULL;
     TARGET_TYPE *MMTkToPrm_po = NULL;
-    int _indexMap[natoms][3];
     indexMap = new TARGET_TYPE*[(natoms)];
     PrmToAx_po = new TARGET_TYPE[natoms];
     MMTkToPrm_po = new TARGET_TYPE[natoms];
 
-    for(int i=0; i<natoms; i++){
-      order[i] = i; // instead of MMTK
-    }
-    for(int i=0; i<natoms; i++){
-        _indexMap[i][2] = order[i];
-    }
-    order[natoms] = 1;
-    order[natoms+1] = 1945;
-    acceptance = order[natoms];
-
     // Set the shared memory size (SHMSZ)
 
     int natoms3 = 3*(natoms);
-    int arrays_cut = 2 + 4*natoms3;
 
     SHMSZ = (
         2*sizeof(TARGET_TYPE) +       // Counter and flag
@@ -128,14 +108,6 @@ int main(int argc, char **argv)
 
     // Build Gmolmodel simulation world
 
-    //World *p_world = new World(
-    //    mol2FN, rbFN, gaffFN, frcmodFN,
-    //    ictd, 
-    //    PrmToAx_po, MMTkToPrm_po,
-    //    shm
-    //);
-
-    
     World *p_world = new World(amberReader, rbFN, flexFN, ictd, PrmToAx_po, MMTkToPrm_po,
         shm); // ELIZA
 
@@ -143,37 +115,7 @@ int main(int argc, char **argv)
 
     srand (time(NULL));
 
-    // Load initial shared memory values
-
-    bool world_initialized = false;
-
-    // Assign convenient pointers for order
-
-    // Assign order in shm[][2] 
-
-    // Get coordinates in the same order as in mol2
-
-    // Get velocities
-
-    // Get order    
-
-    // Get forces   
-
-    // Get simulation parameters
-
-    // Set the client flag
-
-    // Assign convenient pointers for coordinates
-
-    // Assign convenient pointers for velocities
-
-    // Assign convenient pointers for gradients
-
-    // Rewrite indexMap to memory
-
-    // Initialize Simulation
-
-    TARGET_TYPE timestep, mytimestep;
+    TARGET_TYPE mytimestep;
     mytimestep = 0.0015;
 
     // Aloc necessary memory for InitSimulation - temporary
@@ -187,16 +129,15 @@ int main(int argc, char **argv)
     SimTK::Real **inivels;
     inivels = new SimTK::Real*[p_world->mr->natoms]; // ELIZA
     for(int j=0; j<natoms; j++){inivels[j] = new SimTK::Real[3];}
-    //SimTK::Real **indexMap;
     indexMap = new SimTK::Real*[p_world->mr->natoms]; // ELIZA
     for(int j=0; j<natoms; j++){indexMap[j] = new SimTK::Real[3];}
     SimTK::Real **grads;
     grads = new SimTK::Real*[p_world->mr->natoms]; // ELIZA
     for(int j=0; j<natoms; j++){grads[j] = new SimTK::Real[3];}
 
+    // Initialize World
 
     p_world->InitSimulation(coords, vels, inivels, indexMap, grads, mytimestep, true);
-    world_initialized = true;
 
     // Initialize sampler
     HamiltonianMonteCarloSampler *p_HMCsampler = new HamiltonianMonteCarloSampler(p_world->system, p_world->matter, p_world->lig1, p_world->ts);
@@ -205,9 +146,7 @@ int main(int argc, char **argv)
 
     // Options for mass matrix, Lennard Jones
 
-    TARGET_TYPE temp_arg;
-    TARGET_TYPE ts;
-    int pyseed;
+    int pyseed = 0;
     int _massMatNumOpt = 1; // EU
     int _metroFixmanOpt = 1; // EU
     double _lj14sf = 1; //--
@@ -233,32 +172,10 @@ int main(int argc, char **argv)
 
     std::cout << std::fixed;
     std::cout << std::setprecision(4);
-    SimTK::Real timeToReach = 0.001;
-    const SimTK::State& tsState = world->ts->getState(); // less than or equal to integ advanced state
     SimTK::State& integAdvancedState = world->integ->updAdvancedState();
-    //SimTK::State& tsState = world->ts->updState();
-    //world->ts->initialize(tsState);
     for(int i = 0; i<30; i++){
-        // -- STEPTO -- 
-
-        //std::cout << "Time before stepping: " << world->ts->getTime()
-                  //<< "; integAdvancedState Stage before stepping: " 
-                  //<< (((SimTK::Subsystem *)(world->matter))->getStage(integAdvancedState)).getName() 
-                  //<< "; integAdvancedState Stage before stepping: " 
-                  //<< (((SimTK::Subsystem *)(world->matter))->getStage(integAdvancedState)).getName() 
-                  //<< std::endl;
-
-        //world->ts->initialize(integAdvancedState);
         //world->ts->stepTo(timeToReach);
         //world->integ->reinitialize(SimTK::Stage::Instance, true);
-
-        //std::cout << "Time after  stepping: " << world->ts->getTime()
-                  //<< "; integAdvancedState Stage after stepping: " 
-                  //<< (((SimTK::Subsystem *)(world->matter))->getStage(integAdvancedState)).getName() 
-                  //<< "; integAdvancedState Stage before stepping: " 
-                  //<< (((SimTK::Subsystem *)(world->matter))->getStage(integAdvancedState)).getName() 
-                  //<< std::endl;
-
 
         // -- UPDATE --
         std::cout << "=========================================" << std::endl;
@@ -281,9 +198,6 @@ int main(int argc, char **argv)
                   << std::endl;
         //writePdb(*((SimTK::Compound *)(world->lig1)), integAdvancedState, "pdbs", "sb_", 8, "MCs", i);
     }
-
-
-    //delete p_HMCsampler;
 
 }
 
