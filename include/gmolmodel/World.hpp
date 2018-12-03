e634a79e28f8f6aca79b91d50c66938c4f456f63
spiridon_laurentiu <spiridon.laurentiu@gmail.com>
2018-12-03 13:39:44 +0200

    Commented World.hpp.

--- include/gmolmodel/World.hpp
+++ include/gmolmodel/World.hpp
@@ -46,6 +46,7 @@
 #include "Topology.hpp"
 #include "bArgParser.hpp"
 #include "HamiltonianMonteCarloSampler.hpp"
+#include "ConformationalSearch.hpp"
 
 //typedef double Vector3[3];
 
@@ -72,86 +73,91 @@ void writePdb(SimTK::PdbStructure pdb, const char *FN);
 class World{
 public: 
     // --- Structural functions ---
-    // Constructor 
+    /** Constructor **/
     World(int worldIndex, bool isVisual=true, SimTK::Real visualizerFrequency = 0.0015);
  
-    // Creates a topology object and based on amberReader forcefield parameters
-    //  - defines Biotypes; - adds BAT parameters to DuMM
+    /** Creates a topology object and based on amberReader forcefield 
+     parameters - defines Biotypes; - adds BAT parameters to DuMM **/
     void AddMolecule(readAmberInput *amberReader, std::string rbFN, std::string flexFN, std::string ictdF);
  
-    // Intializes simulation parameters.
-    // Beside passing the integrator step and Fixman torque option,
-    // it also initializes default scaling factors for Amber
+    /** Intializes simulation parameters.
+    Beside passing the integrator step and Fixman torque option,
+    it also initializes default scaling factors for Amber **/
     void ModelTopologies(bool useFixmanTorque = false);
     //...............
 
     // --- Mixing functions: Pass configurations among Worlds
-    // Get the current Compound Cartesian coords
+    /** Get the current Compound Cartesian coords **/
     std::vector< std::vector< std::pair<bSpecificAtom *, SimTK::Vec3> > >  getAtomsLocationsInGround(const SimTK::State&);
 
-    // Set Compound, MultibodySystem and DuMM configurations according to
-    // some other World's atoms
+    /** Set Compound, MultibodySystem and DuMM configurations according to
+    some other World's atoms **/
     SimTK::State& setAtomsLocationsInGround(SimTK::State&, std::vector< std::vector< std::pair<bSpecificAtom *, SimTK::Vec3> > > otherWorldsAtomsLocations);
   
-    // Update Gmolmodel bSpecificAtom Cartesian coordinates according to
-    // Molmodel Compound
+    /** Update Gmolmodel bSpecificAtom Cartesian coordinates according to
+    Molmodel Compound **/
     void updateAtomLists(const SimTK::State&);
 
-    // To be called before use of getXs, getYs or getZs
+    /** To be called before use of getXs, getYs or getZs **/
     void updateCoordBuffers(void);
 
-    // Get the coordinates buffers
+    /** Get the coordinates buffers **/
     std::vector<SimTK::Real> getXs(void);
     std::vector<SimTK::Real> getYs(void);
     std::vector<SimTK::Real> getZs(void);
 
-    // Access to molecule (Topology) objects 
-    // Get a readble reference of one of the molecules 
+    /** Access to molecule (Topology) objects 
+    Get a readble reference of one of the molecules **/
     const Topology& getTopology(int moleculeNumber) const;
 
-    // Get a writeble reference of one of the molecules 
+    /** Get a writeble reference of one of the molecules **/
     Topology& updTopology(void);  
     //...............
    
+    //.......................
     // --- Thermodynamics ---
-    // Get macro temperature
+    //.......................
+    /** Get macro temperature **/
     SimTK::Real getTemperature(void);
 
-    // Set macro temperature
+    /** Set macro temperature **/
     void setTemperature(SimTK::Real);
     //...............
 
     // --- Simulation ---
-    // Amber like scale factors.
+    /** Amber like scale factors. **/
     void setAmberForceFieldScaleFactors(void);
 
-    // Set a global scaling factor for the forcefield
+    /** Set a global scaling factor for the forcefield **/
     void setGlobalForceFieldScaleFactor(SimTK::Real);
 
-    // Set GBSA implicit solvent scale factor
+    /** Set GBSA implicit solvent scale factor **/
     void setGbsaGlobalScaleFactor(SimTK::Real);
 
     SimTK::DuMMForceFieldSubsystem * updForceField(void);
 
-    // Use the Fixman torque as an additional force subsystem
+    /** Use the Fixman torque as an additional force subsystem **/
     void useFixmanTorque(void);
     bool isUsingFixmanTorque(void);
     //...............
 
+    //...................
     // --- Statistics ---
-    // How many samples did we have so far
+    //...................
+    /** How many samples did we have so far **/
     int getSampleNumber(void);
 
-    // Sampler manipulation functions
+    /** Sampler manipulation functions **/
     int getNofSamplers(void);
 
+    /** Add a sampler to the World **/
     int addSampler(std::string);
     int addSampler(SamplerName);
 
-    // Get a sampler based on its position in the samplers vector
+    /** Get a sampler based on its position in the samplers vector **/
     const HamiltonianMonteCarloSampler * getSampler(int which);
 
-    // Get a writable sampler based on its position in the samplers vector
+    /** Get a writable sampler based on its position in the samplers vector **/
     HamiltonianMonteCarloSampler * updSampler(int which);
 
     FixmanTorque * updFixmanTorque(void);
@@ -160,10 +166,10 @@ public:
     //...............
   
     // -- Debugging / helper functions ---
-    // Print information about Simbody systems
+    /** Print information about Simbody systems **/
     void PrintSimbodyStateCache(SimTK::State&);
   
-    // Print Compound Cartesian coordinates
+    /** Print Compound Cartesian coordinates **/
     void printPoss(const SimTK::Compound& c, SimTK::State& someState);
     //...............
  
@@ -177,43 +183,44 @@ public:
 public:
 
     // --- Structural data ---
-    // System->MultibodySystem->MolecularMechanicsSystems->CompoundSystem
+    /** System->MultibodySystem->MolecularMechanicsSystems->CompoundSystem **/
     SimTK::CompoundSystem *compoundSystem;
 
-    // Subsystem->SimbodyMatterSubsystem
+    /** Subsystem->SimbodyMatterSubsystem **/
     SimTK::SimbodyMatterSubsystem *matter;
 
-    // Subsystem->ForceSubsystem->GeneralForceSubsystem
+    /** Subsystem->ForceSubsystem->GeneralForceSubsystem **/
     SimTK::GeneralForceSubsystem *forces;
 
-    // Force::Custom
+    /** Get writble pointer to Fixman Torque and other forces**/
     FixmanTorque * FixmanTorqueImpl;
     SimTK::Force::Custom *ExtForce;
 
-    // Subsystem->ForceSubsystem->DuMMForceFieldSubsystem
+    /** Subsystem->ForceSubsystem->DuMMForceFieldSubsystem **/
     SimTK::DuMMForceFieldSubsystem *forceField;
 
-    // Nof molecules
+    /** Nof molecules **/
     int moleculeCount;
 
-    // Filenames for files needed to build molecules (topologies)
+    /** Filenames for files needed to build molecules (topologies) **/
     std::string rbFN; // rigid bodies specifications
     std::string frcmodF; // to be removed
     std::string flexFN; // flexible bonds specifications
     std::string ictdF; // regimen specification
   
-    // Molecules (topologies<-Compounds) objects
+    /** Molecules (topologies<-Compounds) objects **/
     std::vector<bMoleculeReader *> moleculeReaders;
     std::vector<Topology *> topologies;
 
+    /** Vectors of Cartesian coordinates **/
     std::vector<SimTK::Real> Xs;
     std::vector<SimTK::Real> Ys;
     std::vector<SimTK::Real> Zs;
 
-    // This vector stores a configuration if is needed for later use
+    /** This vector stores a configuration if is needed for later use **/
     SimTK::Transform *TVector;
   
-    // Topologies graphs as tables - to be removed
+    /** Topologies graphs as tables - to be removed **/
     int **mbxTreeMat;    // tree representing the bonding
     SimTK::Real *branchMassVec; // branch masses self body included
     //...............
@@ -226,6 +233,7 @@ public:
     SimTK::VerletIntegrator *integ;
     SimTK::TimeStepper *ts;
     std::vector<HamiltonianMonteCarloSampler *> samplers;
+    //std::vector<Sampler *> samplers;
     bool _useFixmanTorque;
     //...............
 
