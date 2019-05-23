9770736a792a29284e6740ccf079b7cccdce1b56
Laurentiu Spiridon <spiridon.laurentiu@gmail.com>
2019-05-23 12:48:16 +0300

    Untrack all unnecessary tests.

--- tests/TestMassSqrtAndDet.cpp
+++ tests/TestMassSqrtAndDet.cpp
@@ -21,15 +21,12 @@ using namespace SimTK;
 int main() {
 try {
     
-    // MOLMODEL CONSTRUCTORS DON'T WORK IN Realease mode:
     CompoundSystem system; // Extends MolecularMechanicsSystem (Molmodel)
     SimbodyMatterSubsystem matter(system);
-    DecorationSubsystem decorations(system);
     DuMMForceFieldSubsystem forceField(system);
     forceField.loadAmber99Parameters();
     Protein protein("G");
     protein.assignBiotypes();
-    //Ethane protein;
     system.adoptCompound(protein);
 
     for (unsigned int r=0 ; r<protein.getNumBonds(); r++){
@@ -38,7 +35,6 @@ try {
 
     system.modelCompounds(); 
     system.addEventReporter(new Visualizer::Reporter(system, 0.020));
-    //system.addEventHandler(new VelocityRescalingThermostat(	   system,  293.15, 0.1));
     State state = system.realizeTopology();
     LocalEnergyMinimizer::minimizeEnergy(system, state, 15.0);
     VerletIntegrator integ(system);
@@ -69,7 +65,6 @@ try {
     // Get detM
     SimTK::Real detM = 1.0;
     SimTK::Vector DetV(nu);
-    //SimTK::Matrix D0(6, 6);
     Real* newDetM = new Real(1.0);
 
     matter.calcDetM(state, V, DetV, newDetM);
@@ -93,39 +88,6 @@ try {
     ts.initialize(state);
     ts.stepTo(0.12); // 0.12ps
 
-//    // Check Fixman torque
-//    unsigned int nq = state.getNQ();
-//    SimTK::Vector Qs = state.getQ();
-//    SimTK::Vector copyQs(nq);
-//    for(int i = 0; i < nq; i++){copyQs[i] = Qs[i];} 
-//    std::cout << "Qs: " << Qs << std::endl;
-//    SimTK::Real prevQ = 0, prevFixmanPotential = 0, FixmanPotential = 0;
-//    SimTK::Real dQ = 0, dFixman = 0, deriv = 0;
-//    SimTK::Real TinyIncrement = 0.0001;
-//
-//    SimTK::Vector V1(nu);
-//    SimTK::Vector V2(nu); // This stores the torques.
-//    for(int qIx=0; qIx<nq; qIx++){
-//        for(int i=0; i<10; i++){
-//            matter.calcDetM(state, V, DetV, newDetM);
-//            prevFixmanPotential = FixmanPotential;
-//            SimTK::Real FixmanPotential = 0.5 * std::log(*newDetM);
-//            dFixman = FixmanPotential - prevFixmanPotential;
-//
-//            prevQ = Qs[qIx];
-//            copyQs[qIx] += TinyIncrement;
-//            state.setQ(copyQs);
-//
-//            system.realize(state, SimTK::Stage::Dynamics);
-//
-//            matter.calcFixmanTorque(state, V1, V2, newDetM);
-//            dQ = Qs[qIx] - prevQ;
-//            deriv = dFixman / dQ;
-//            std::cout << "calcFixmanTorque V2:" << V2 << std::endl;
-//            std::cout << "numDeriv:" << deriv << std::endl;
-//        }
-//    }
-//
     return 0;
 } 
 catch(const std::exception& e) {
